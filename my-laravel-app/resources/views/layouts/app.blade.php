69f51ac80573260408f0675f6f2077f6943040c1
masal9pse <returnymgstokh@icloud.com>
2020-09-18 20:35:24 +0900

    fix button

--- my-laravel-app/resources/views/layouts/app.blade.php
+++ my-laravel-app/resources/views/layouts/app.blade.php
@@ -1,29 +1,33 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>MY FAVARITE BOOKS</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link rel="stylesheet" href="/asset/css/style.css">
-    </head>
-    <body>
-        <header>
-            <div class="header-wrapper">
-                <div class="title"><a href="{{ route('home') }}">MY FAVARITE BOOK</a></div>
-                <div class="menu">
-                    <ul>
-                        <li><a href="{{ route('register_book') }}">登録</a></li>
-                        <li><a href="{{ route('show_books')}}">一覧</a></li>
-                    </ul>                
-                </div>
+    <title>MY FAVARITE BOOKS</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link rel="stylesheet" href="/asset/css/style.css">
+</head>
+
+<body>
+    <header>
+        <div class="header-wrapper">
+            <div class="title"><a href="{{ route('home') }}">MY FAVARITE BOOK</a></div>
+            <div class="menu">
+                <ul>
+                    <li><a href="{{ route('register_book') }}">登録ボタン</a></li>
+                    <li><a href="{{ route('show_books')}}">一覧ボタン</a></li>
+                </ul>
             </div>
-        </header>
-        <body>
-            @yield('content')
-        </body>
+        </div>
+    </header>
+
+    <body>
+        @yield('content')
     </body>
+</body>
+
 </html>
